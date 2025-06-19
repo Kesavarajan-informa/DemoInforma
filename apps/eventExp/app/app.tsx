53762d5b472fb83eb0b73285fff43cddb58c1ae0
Devanand <jayakumar.devanand@informa.com>
2025-06-19 19:20:37 +0530

    Initial commit

---
+++ apps/eventExp/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@org-informa/eventExp" />
+    </div>
+  );
+}
+
+export default App;
