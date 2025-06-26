74db59aa0341d6ca4b9503df816329b5fcc9ab60
Devanand <jayakumar.devanand@informa.com>
2025-06-26 05:39:21 +0530

    feat(eventExp):implemented new feature 1

--- apps/eventExp/app/app.tsx
+++ apps/eventExp/app/app.tsx
@@ -1,9 +1,11 @@
+import LoginPage from './LoginPage';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="@org-informa/eventExp" />
+      <LoginPage />
     </div>
   );
 }
