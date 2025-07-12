37b1be0a3e9048bf574948a012c26e7766dfe7e7
KiranShigli <KiranShigli@informa.com>
2025-07-12 17:15:59 +0100

    try allure

---
+++ apps/eventExp-e2e/src/e2e/chphiform.cy.ts
@@ -0,0 +1,17 @@
+describe('CPHI Form ', ()=>{
+
+    it ('Fill the all details', ()=>{
+
+    cy.visit('https://www.cphi-online.com/live/service/subscribe46_step2.jsp');
+    // cy.get('#onetrust-accept-btn-handler#onetrust-accept-btn-handler').click();
+    // cy.get('#close-button').first().click();
+    // cy.get('[type="email"]').type('rajamohana99@gmail.com');
+    // cy.get('[name="FIRSTNAME"]').type('raja');
+    // cy.get('[name="LASTNAME"]').type('mohan');
+    // cy.get('#SubscribeFormCountrySelect').select('Angola').should('contain', 'Angola');
+    // cy.get("input[name='SUBSCRIBERREGIONINTEREST'][value='3']").click({force: true});
+    // cy.get('[name="SUBSCRIBERTOPICINTEREST"][value="6"]').click({force: true});
+    // cy.get('[class="check__mark"]').first().click();
+    
+    })
+})
